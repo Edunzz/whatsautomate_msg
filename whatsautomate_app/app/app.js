7f443ff0d3491b11eab91e7834b119826491ae36
root <root@DT-043165223057.clients.dynatrace.org>
2023-01-07 13:30:22 -0500

    update

--- whatsautomate_app/app/app.js
+++ whatsautomate_app/app/app.js
@@ -1,3 +1,4 @@
+// frontend
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -8,6 +9,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/autorizar', (req, res) => {
+  client.initialize();
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+});
+
+// whatsapp api node js
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+  qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
 });
